73f73e2c336160a481b943ebbced6988a0f8f921
symsymmas <ccimia@ippon.fr>
2014-04-03 11:56:00 +0200

    FirstCommit

---
+++ Parameters_Jacko/Parameters_Jacko/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Parameters_Jacko.Resources;
+
+namespace Parameters_Jacko
+{
+    /// <summary>
+    /// Permet d'accéder aux ressources de chaîne.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
